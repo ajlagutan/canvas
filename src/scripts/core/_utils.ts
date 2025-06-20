5cc6adf8651b80c44b4000c0a7adf9b24285c5d7
AJ Lagutan <alvin.lagutan@ghl.com>
2025-06-20 20:20:50 +0800

    Update logger functions

--- src/scripts/core/_utils.ts
+++ src/scripts/core/_utils.ts
@@ -6,6 +6,14 @@
  * @namespace
  */
 export namespace logger {
+  enum levels {
+    trace = 0,
+    debug = 1,
+    info = 2,
+    warn = 4,
+    error = 8,
+    none = 16,
+  }
   /**
    * Debug level log.
    *
@@ -14,7 +22,7 @@ export namespace logger {
    * @param data The data to write to the browser console.
    */
   export function debug(this: any, ...data: any[]): void {
-    console.debug(`[${this.name}]`, ...data);
+    _log.call(this, levels.debug, ...data);
   }
   /**
    * Error level log.
@@ -24,7 +32,7 @@ export namespace logger {
    * @param data The data to write to the browser console.
    */
   export function error(this: any, ...data: any[]): void {
-    console.error(`[${this.name}]`, ...data);
+    _log.call(this, levels.error, ...data);
   }
   /**
    * Information level log.
@@ -34,16 +42,73 @@ export namespace logger {
    * @param data The data to write to the browser console.
    */
   export function info(this: any, ...data: any[]): void {
-    console.info(`[${this.name}]`, ...data);
+    _log.call(this, levels.info, ...data);
+  }
+  /**
+   * Log to browser console.
+   *
+   *
+   *
+   * @param data The data to write to the browser console.
+   */
+  export function log(this: any, ...data: any[]): void {
+    _log.call(this, levels.none, ...data);
+  }
+  /**
+   * Trace level log.
+   *
+   *
+   *
+   * @param data The data to write to the browser console.
+   */
+  export function trace(this: any, ...data: any[]): void {
+    _log.call(this, levels.trace, ...data);
   }
   /**
    * Warning level log.
-   * 
-   * 
-   * 
+   *
+   *
+   *
    * @param data The data to write to the browser console.
    */
   export function warn(this: any, ...data: any[]): void {
-    console.warn(`[${this.name}]`, ...data);
+    _log.call(this, levels.warn, ...data);
+  }
+  /**
+   * Logs a data to the browser console with level category.
+   *
+   *
+   *
+   * @param level Log level category.
+   * @param data The data to write to the browser console.
+   */
+  function _log(this: any, level: levels, ...data: any[]): void {
+    let fn;
+    switch (level) {
+      case levels.trace:
+        fn = console.trace;
+        break;
+      case levels.debug:
+        fn = console.debug;
+        break;
+      case levels.info:
+        fn = console.info;
+        break;
+      case levels.warn:
+        fn = console.warn;
+        break;
+      case levels.error:
+        fn = console.error;
+        break;
+      case levels.none:
+      default:
+        fn = console.log;
+        break;
+    }
+    if (this.name) {
+      fn(`[${this.name}]`, ...data);
+      return;
+    }
+    fn(...data);
   }
 }
